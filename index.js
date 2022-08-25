27e8ebd6f6a21508a99b9d0ee5995f0d7e637f53
Rioba-Ian-Felix <riobafelix@gmail.com>
2022-08-25 21:36:36 +0300

    "displayed data from JS"

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
 console.log("hello mum.")
-const canvasChart = document.getElementById("myChart").getContext('2d')
-
 import data from "./data.json" assert {type: "json"}
 
 console.log(data)
 
-// for (let i = 0; i < data.length; i++) {
-//     console.log(data[i].day)
-// }
-
 const days = data.map(item => item.day)
 const amount = data.map(item => item.amount)
 
-days.sum()
 
-console.table(days.sum(), amount)
+console.table(days, amount)
 
-// const myChart = new Chart(canvasChart, {
-//     type: 'bar',
-//     data: {
-//         labels: days,
-//         datasets: [{
-//             label: "days of the week",
-//             data: Object.values(amount)
-//         }
-//         ]
-//     }
-// })
+const displayExpenses = () => {
+    let dataList = ''
+
+
+    dataList += `
+    <ul>
+    <li><span>days: ${data.map(item => item.day)}</span></li>
+    <li><span>amount: ${data.map(item => item.amount)}</span></li>
+    </ul>
+    `
+    document.getElementById('chart').innerHTML = dataList
+
+}
+
+displayExpenses()
