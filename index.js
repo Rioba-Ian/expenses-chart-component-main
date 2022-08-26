516bca764ac83b04d0f3cbd67f055c5bdfe26c82
Rioba-Ian-Felix <riobafelix@gmail.com>
2022-08-26 16:29:42 +0300

    "added chart"

--- index.js
+++ index.js
@@ -1,26 +1,56 @@
-console.log("hello mum.")
 import data from "./data.json" assert {type: "json"}
 
-console.log(data)
 
-const days = data.map(item => item.day)
-const amount = data.map(item => item.amount)
+data.map(item => {
+    const chartContainer = document.getElementById("chart")
+    const barContainer = document.createElement('div')
+    const bar = document.createElement('div');
+    const day = document.createElement('p')
+
+    const dayWeek = item.day
+    const barHeight = `${(item.amount / 7)}rem`
+
+    // bar
+    bar.classList.add('bar');
+    bar.style.height = barHeight
+
+    // day
+    day.classList.add('day')
+    day.innerText = dayWeek
+
+    // bar container
+    barContainer.classList.add('bar-container')
+
+    barContainer.appendChild(bar)
+    barContainer.appendChild(day)
+
+    chartContainer.append(barContainer)
+})
 
 
-console.table(days, amount)
+/*
+This was an initial method I had tried to display the data
 
-const displayExpenses = () => {
-    let dataList = ''
+*/
+
+// const days = data.map(item => item.day)
+// const amount = data.map(item => item.amount)
 
 
-    dataList += `
-    <ul>
-    <li><span>days: ${data.map(item => item.day)}</span></li>
-    <li><span>amount: ${data.map(item => item.amount)}</span></li>
-    </ul>
-    `
-    document.getElementById('chart').innerHTML = dataList
+// console.table(days, amount)
 
-}
+// const displayExpenses = () => {
+//     let dataList = ''
 
-displayExpenses()
+
+//     dataList += `
+//     <ul>
+//     <li><span>days: ${data.map(item => item.day)}</span></li>
+//     <li><span>amount: ${data.map(item => item.amount)}</span></li>
+//     </ul>
+//     `
+//     document.getElementById('chart').innerHTML = dataList
+
+// }
+
+// displayExpenses()
